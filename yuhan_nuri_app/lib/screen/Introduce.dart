084f97e4865e9976eeca2d380681a3cfd2f6cd78
KimTaewoo <xass1995@gmail.com>
2020-12-04 15:13:06 +0900

    Introduce Text and Image Change

--- yuhan_nuri_app/lib/screen/Introduce.dart
+++ yuhan_nuri_app/lib/screen/Introduce.dart
@@ -27,58 +27,58 @@ class IntroduceScreenState extends State<IntroduceScreen> {
 
   @override
   Widget build(BuildContext context) {
-    const bodyStyle = TextStyle(fontSize: 19.0);
-    const pageDecoration = const PageDecoration(
+    PageDecoration pageDecoration = PageDecoration(
       titleTextStyle: TextStyle(
-          fontSize: 28.0,
+          fontSize: 35.0,
           fontWeight: FontWeight.w700,
-          height: 5,
-          textBaseline: TextBaseline.alphabetic),
-      bodyTextStyle: bodyStyle,
+          fontFamily: 'jua',
+          color: Colors.white), //tile font size, weight and color
+      bodyTextStyle:
+          TextStyle(fontSize: 19.0, color: Colors.white, fontFamily: 'jua'),
+      //body text size and color
       descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-      pageColor: Colors.white,
-      imagePadding: EdgeInsets.zero,
+      //decription padding
+      imagePadding: EdgeInsets.all(20), //image padding
+      boxDecoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          stops: [0.1, 0.5, 0.7, 0.9],
+          colors: [
+            Color(0xFF71A7D0),
+            Color(0xFF8CC3D9),
+            Color(0xFF81C0D5),
+            Color(0xFF85D1D6),
+          ],
+        ),
+      ), //show linear gradient background of page
     );
-
     // 각각 페이지들을 배열처럼 저장
     return IntroductionScreen(
       key: introKey,
       pages: [
         PageViewModel(
-          title: "안녕하세용",
+          title: "환영합니다",
           image: Image(
-            image: AssetImage("assets/hi.png"),
+            image: AssetImage("assets/nuriicon.png"),
           ),
-          body: "유한 대학교 심리 상담 센터 예약 앱 유한누리입니다.",
+          body: "안녕하세요! \n 심리상담센터 예약어플 \n 유한 누리입니다. \n 유한 누리 방문이 처음이신가요? ",
           decoration: pageDecoration,
         ),
         PageViewModel(
-          title: "하단 탭",
-          body: "",
-          // footer: RaisedButton(
-          //   onPressed: () {
-          //     introKey.currentState?.animateScroll(0);
-          //   },
-          //   child: const Text(
-          //     'Button',
-          //     style: TextStyle(color: Colors.black),
-          //   ),
-          //   color: Colors.amber,
-          //   shape: RoundedRectangleBorder(
-          //       borderRadius: BorderRadius.circular(8.0)),
-          // ),
+          title: "무엇이 고민인가요?",
+          image: Image(
+            image: AssetImage("assets/nuriicon.png"),
+          ),
+          body: "유한 누리로 간편하게 \n 심리검사와 상담예약을 해보세요. \n 비밀을 지켜드려요!",
           decoration: pageDecoration,
         ),
         PageViewModel(
-          title: "마이 페이지",
-          bodyWidget: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("테스트1", style: bodyStyle),
-              Text("테스트2", style: bodyStyle),
-              Text("테스트3", style: bodyStyle),
-            ],
+          title: "유한 누리",
+          image: Image(
+            image: AssetImage("assets/nuriicon.png"),
           ),
+          body: "유한 누리를 이용할 준비가 되셨나요? \n 그럼 지금바로 시작해보세요! \n 상담센터는 언제나 환영입니다!",
           decoration: pageDecoration,
         ),
       ],
@@ -88,7 +88,7 @@ class IntroduceScreenState extends State<IntroduceScreen> {
       nextFlex: 0,
       skip: const Text('Skip'),
       next: const Icon(Icons.arrow_forward),
-      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      done: const Text('시작하기', style: TextStyle(fontWeight: FontWeight.w600)),
       dotsDecorator: const DotsDecorator(
         size: Size(10.0, 10.0),
         color: Color(0xFFBDBDBD),

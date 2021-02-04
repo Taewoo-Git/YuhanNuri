0e6fe08e9e7e83150f65a506a78eb1f77da43a14
KimTaewoo <xass1995@gmail.com>
2021-02-05 04:49:27 +0900

    edit for compatibility in app

--- yuhan_nuri_app/lib/screen/YuhanNuri.dart
+++ yuhan_nuri_app/lib/screen/YuhanNuri.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:vibration/vibration.dart';
@@ -17,13 +16,14 @@ CookieManager cm;
 
 // [0] 메인, [1] 예약, [2] 문의, [3] 마이페이지, [4] 채팅, [5] 만족도조사페이지
 // urls 배열 외의 외부url을 로드할 시 webview가 아닌 기기의 브라우저(크롬, 사파리)를 이용해 로드(하이퍼링크 등)
+const Domain = 'https://counsel.yuhan.ac.kr/';
 const urls = [
-  'http://counsel.yuhan.ac.kr/',
-  'http://counsel.yuhan.ac.kr/user/reservation',
-  'http://counsel.yuhan.ac.kr/user/question',
-  'http://counsel.yuhan.ac.kr/user/mypage',
-  'http://counsel.yuhan.ac.kr/user/mypage?chatting',
-  'http://counsel.yuhan.ac.kr/user/satisfaction',
+  Domain,
+  Domain + 'user/reservation',
+  Domain + 'user/question',
+  Domain + 'user/mypage',
+  Domain + 'user/mypage?chatting',
+  Domain + 'user/satisfaction',
 ];
 
 class YuhanNuri extends StatefulWidget {
@@ -73,33 +73,6 @@ class YuhanNuriState extends State<YuhanNuri> {
         });
 
     cm = new CookieManager();
-
-    KeyboardVisibilityController().onChange.listen((bool visible) {
-      if (visible) {
-        // 키보드 올라왔을 때
-        _webViewController.evaluateJavascript(
-            source: 'document.activeElement.scrollIntoView({block: "center"})');
-        /*_webViewController.getUrl().then((url) => {
-              if (url != urls[3])
-                {
-                  _webViewController.evaluateJavascript(
-                      source:
-                          'document.activeElement.scrollIntoView({block: "center"})')
-                }
-            });*/
-      } else {
-        // 키보드가 내려갈 때
-        _webViewController.evaluateJavascript(
-            source: 'document.activeElement.blur()');
-        /*_webViewController.getUrl().then((url) => {
-              if (url != urls[3])
-                {
-                  _webViewController.evaluateJavascript(
-                      source: 'document.activeElement.blur()')
-                }
-            });*/
-      }
-    });
   }
 
   void gotoPage(String msg) {
@@ -158,7 +131,6 @@ class YuhanNuriState extends State<YuhanNuri> {
                     initialHeaders: header,
                     onLoadStart: (_webViewController, String url) {
                       if (!urls.contains(url)) {
-                        //google form 단축 url일 경우
                         if (url.contains(
                             'action=com.google.firebase.dynamiclinks.VIEW_DYNAMIC_LINK;')) {
                           url = url
